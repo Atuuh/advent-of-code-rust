53fedfff2d20a70433a1dcc7995cbb9385d9386c
Atuuh <not-another-one@hotmail.com>
2025-07-29 20:54:43 +0000

    add scrappy solution for 2017 day 1

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,3 +1,32 @@
+use std::{fs, path::Path};
+
+use crate::year2017day01::{solve};
+
+pub enum AocError {
+    Allsorts
+}
+
+pub mod year2017day01;
+
 fn main() {
-    println!("Hello, world!");
+    let year = 2017;
+    let day = 1;
+    let input_result= get_input(year, day);
+
+    let result = input_result.and_then(solve);
+    match result {
+        Ok(value) => {
+            println!("Answer is {value}")
+        },
+        Err(_) => {
+            println!("Failed to work out answer")
+        },
+    }
+}
+
+fn get_input(year: u16, day: u8) -> Result<String, AocError> {
+    let path_string = format!("./inputs/{year}/day-{day:0>2}.txt");
+    let path = Path::new(&path_string);
+    let result = fs::read_to_string(path).map(|s | s.trim().to_owned());
+    result.map_err(|_e | AocError::Allsorts)
 }
---
+++ aoc/src/year2017day01.rs
@@ -0,0 +1,20 @@
+use crate::AocError;
+
+pub fn solve(input: String) -> Result<String, AocError> {
+    let numbers: Vec<u32> = input
+        .chars()
+        .map(|char| char.to_digit(10))
+        .flatten()
+        .collect();
+    let offset = numbers.len() / 2;
+    let zipped = zip_self_with_offset(&numbers, offset);
+    let result = zipped.fold(0, |a, (x, y)| if x == y { a + x } else { a });
+    Ok(result.to_string())
+}
+
+fn zip_self_with_offset<T>(xs: &Vec<T>, offset: usize) -> impl Iterator<Item = (&T, &T)> {
+    let len = xs.len();
+    xs.iter()
+        .enumerate()
+        .map(move |(i, x)| (x, &xs[(i + offset) % len]))
+}
