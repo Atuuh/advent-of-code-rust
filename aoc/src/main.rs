ae0567ea5998cb0a89b165d78a336da9e58c97c2
Atuuh <not-another-one@hotmail.com>
2025-08-11 19:36:39 +0000

    add 2017 day 6 part 1 solution

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,9 +1,8 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, time::Instant};
 
-use crate::year2017day05::{solve};
-
+#[derive(Debug)]
 pub enum AocError {
-    Allsorts
+    Allsorts,
 }
 
 pub mod year2017day01;
@@ -11,26 +10,28 @@ pub mod year2017day02;
 pub mod year2017day03;
 pub mod year2017day04;
 pub mod year2017day05;
+pub mod year2017day06;
 
 fn main() {
     let year = 2017;
-    let day = 5;
-    let input_result= get_input(year, day);
+    let day = 6;
+    let input_result = get_input(year, day).unwrap();
 
-    let result = input_result.and_then(solve);
-    match result {
-        Ok(value) => {
-            println!("Answer is {value}")
-        },
-        Err(_) => {
-            println!("Failed to work out answer")
-        },
-    }
+    use crate::year2017day06::{parse, part1, part2};
+    let initial_time = Instant::now();
+    let parsed_input = parse(input_result.as_str());
+    let after_parsed_input = Instant::now();
+    let part1_answer = part1(&parsed_input);
+    let after_part1 = Instant::now();
+    let part2_answer = part2(&parsed_input);
+    let after_part2 = Instant::now();
+
+    println!("Part 1: {part1_answer} ({:?}), Part 2: {part2_answer} ({:?})", after_part1.duration_since(after_parsed_input), after_part2.duration_since(after_part1) )
 }
 
 fn get_input(year: u16, day: u8) -> Result<String, AocError> {
-    let path_string = format!("./inputs/{year}/day-{day:0>2}.txt");
+    let path_string = format!("./inputs/{year}/{day:0>2}.txt");
     let path = Path::new(&path_string);
-    let result = fs::read_to_string(path).map(|s | s.trim().to_owned());
-    result.map_err(|_e | AocError::Allsorts)
+    let result = fs::read_to_string(path).map(|s| s.trim().to_owned());
+    result.map_err(|_e| AocError::Allsorts)
 }
---
+++ aoc/src/year2017day06.rs
@@ -0,0 +1,62 @@
+use std::{collections::HashSet, fmt::Display, ops::IndexMut};
+
+pub fn parse(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|x| x.parse::<u32>())
+        .flatten()
+        .collect()
+}
+
+pub fn part1(numbers: &Vec<u32>) -> impl Display {
+    let mut memory = numbers.clone();
+    let mut visited = HashSet::new();
+    let mut index = 0;
+    while !visited.contains(&memory){
+        visited.insert(memory.clone());
+        redistribute_blocks(&mut memory);
+        index += 1;
+    }
+    index
+}
+
+pub fn part2(numbers: &Vec<u32>) -> impl Display {
+    numbers.iter().max().unwrap_or(&5)
+}
+
+fn redistribute_blocks(memory_banks: &mut Vec<u32>) {
+    let length = memory_banks.len();
+    let (index, max_value) = memory_banks
+        .iter()
+        .enumerate()
+        .rev()
+        .max_by(|(_ix, x), (_iy, y)| x.cmp(y))
+        .unwrap().clone();
+
+    let m_index = index.clone();
+    let m_max_value = max_value.clone();
+    memory_banks[m_index] = 0;
+
+    for x in 1..m_max_value + 1 {
+        let target_index = (m_index + x as usize) % length;
+        memory_banks[(target_index) % length] += 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::year2017day06::redistribute_blocks;
+
+    #[test]
+    fn test_redistribute_blocks() {
+        let mut memory = vec![0, 2, 7, 0];
+        redistribute_blocks(&mut memory);
+        assert_eq!(memory, vec![2, 4, 1, 2]);
+        redistribute_blocks(&mut memory);
+        assert_eq!(memory, vec![3, 1, 2, 3]);
+        redistribute_blocks(&mut memory);
+        assert_eq!(memory, vec![0, 2, 3, 4]);
+        redistribute_blocks(&mut memory);
+        assert_eq!(memory, vec![1, 3, 4, 1]);
+    }
+}
